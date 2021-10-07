392b8fb435a5e9d3fc617a0115adbec1d474244b
Dinh Manh Tri <TriDMTH2008060@fpt.edu.vn>
2021-10-07 17:02:48 +0700

    first commit

---
+++ Assignment1/ListViewExample/ListViewExample/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using ListViewExample.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ListViewExample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private List<Student> Students;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Students = StudentManager.GetStudents();
+        }
+
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var student = (Student)e.ClickedItem;
+            ResultTextBlock.Text = "You Selected: " + student.Name;
+        }
+    }
+}
---
+++ Assignment1/ListViewExample/ListViewExample/Model/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewExample.Model
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string YearOfBirth { get; set; }
+        public string Image { get; set; }
+    }
+
+    public class StudentManager
+    {
+        public static List<Student> GetStudents()
+        {
+            var students = new List<Student>();
+
+            students.Add(new Student { StudentId = 1, Name = "Nguyễn Công Phượng", YearOfBirth = "2002", Image = "Assets/1.png" });
+            students.Add(new Student { StudentId = 2, Name = "Nguyễn Tiến Linh", YearOfBirth = "2003", Image = "Assets/2.png" });
+            students.Add(new Student { StudentId = 3, Name = "Đỗ Duy Mạnh", YearOfBirth = "2002", Image = "Assets/3.png" });
+            students.Add(new Student { StudentId = 5, Name = "Đỗ Hùng Dũng", YearOfBirth = "1992", Image = "Assets/5.png" });
+            students.Add(new Student { StudentId = 6, Name = "Vũ Văn Thanh", YearOfBirth = "1993", Image = "Assets/6.png" });
+            students.Add(new Student { StudentId = 7, Name = "Đặng Văn Lâm", YearOfBirth = "1992", Image = "Assets/7.png" });
+            students.Add(new Student { StudentId = 8, Name = "Hà Đức Chinh", YearOfBirth = "1998", Image = "Assets/8.png" });
+            students.Add(new Student { StudentId = 9, Name = "Đoàn Văn Hậu", YearOfBirth = "1890", Image = "Assets/9.png" });
+            students.Add(new Student { StudentId = 10, Name = "Nguyễn Tuấn Anh", YearOfBirth = "1990", Image = "Assets/10.png" });
+            students.Add(new Student { StudentId = 11, Name = "Nguyễn Tấn Trường", YearOfBirth = "1991", Image = "Assets/11.png" });
+            students.Add(new Student { StudentId = 12, Name = "Quế Ngọc Hải", YearOfBirth = "1995", Image = "Assets/12.png" });
+            students.Add(new Student { StudentId = 13, Name = "Bùi Tiến Dũng", YearOfBirth = "1997", Image = "Assets/13.png" });
+
+            return students;
+        }
+    }
+}
+
